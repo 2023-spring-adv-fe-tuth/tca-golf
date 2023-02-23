44f0a2a48d97247c7ae2d085a4196f9384d0a215
skylarNahn <nahn@madisoncollege.edu>
2023-02-23 11:43:44 -0600

    adjusted the sizes of the buttons

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,15 +6,15 @@ export const HomePage = () => {
         <h1>TCA Golf Companion App</h1><br/>
 
         <input type='text' placeholder="Player One"/>
-        <Button variant="outline-primary">Enter</Button>
+        <Button variant="outline-primary" size="sm">Enter</Button>
         
         <br/><br/>
 
         <input type='text' placeholder="Player Two"/>
-        <Button variant="outline-primary">Enter</Button>
+        <Button variant="outline-primary" size="sm">Enter</Button>
 
         <br/><br/>
-        <Button href="./play" type='submit' variant="success">Play</Button>
+        <Button href="./play" type='submit' variant="success" size="lg">Play</Button>
         </>
             
     );
